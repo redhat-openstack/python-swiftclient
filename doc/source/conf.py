00ef927f51859ed8600dcc3ac8bfa45dc57be06e
Pete Zaitcev <zaitcev@kotori.zaitcev.us>
2014-09-18 19:25:22 -0600

    Patch docs to use oslo.sphinx
    Patching the source this way is the technique that most our projects
    use for now. Some even use sed(1)! Ask Padraig as to why this is.
    Sooner or later we need to migrate to oslosphinx 2.0, but not today.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, ROOT)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-              'sphinx.ext.coverage', 'oslosphinx']
+              'sphinx.ext.coverage', 'oslo.sphinx']
 
 autoclass_content = 'both'
 autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance']
